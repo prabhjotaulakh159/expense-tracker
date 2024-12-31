5102b2dd049680bbc46f4cf5c2a6610de3ba904d
PrabhjotAulakh12345 <prabhjotaulakh68@gmail.com>
2024-12-31 02:51:56 -0500

    copy gravcful shutdown from docs

--- server/main.go
+++ server/main.go
@@ -1,8 +1,46 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	"github.com/gin-gonic/gin"
+)
+
+// https://gin-gonic.com/docs/examples/graceful-restart-or-stop/
 func main() {
 	router := gin.Default()
-	router.Run()
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: router.Handler(),
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+
+	select {
+	case <-ctx.Done():
+		log.Println("timeout of 5 seconds.")
+	}
+	log.Println("Server exiting")
 }
