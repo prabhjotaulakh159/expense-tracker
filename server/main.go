dd3c1b31a5b538ff18619f80b69bd933826acf29
PrabhjotAulakh12345 <prabhjotaulakh68@gmail.com>
2025-01-31 23:01:06 -0500

    create 1st go http server

--- server/main.go
+++ server/main.go
@@ -2,8 +2,44 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"log"
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
+
 func main() {
-	fmt.Println("Hello world!")
+	mux := http.NewServeMux()	
+	server := newServer(mux, "localhost", 3000)
+	go startServer(server)
+	log.Println(fmt.Sprintf("server is listening on %s", server.Addr))
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+	<-quit
+
+	stopServer(server)
+	log.Println("server stopped successfully")
+}
+
+func newServer(mux *http.ServeMux, host string, port int) *http.Server {
+	return &http.Server {
+		Addr: fmt.Sprintf("%s:%d", host, port),
+		Handler: mux,
+	}
+}
+
+func startServer(server *http.Server) {
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("starting server: %v", err)
+	}
+}
+
+func stopServer(server *http.Server) {
+	if err := server.Shutdown(context.Background()); err != nil {
+		log.Fatalf("stopping server: %v", err)
+	}
 }
